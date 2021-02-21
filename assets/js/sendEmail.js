16490befe9676948d06c1668764207fbc734ab11
wellwhynot <wellwhynot@protonmail.com>
2021-02-21 10:01:38 +0000

    Implement EmailJS integration on the contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,14 @@
+function sendMail(contactForm) {
+    emailjs.send("default_service","template_oqj4zaf", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+        console.log("FAILED", error);
+        });
+}
