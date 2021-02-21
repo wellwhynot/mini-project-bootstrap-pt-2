cb0a4f8da3fdc579d0703fe8d55f86c45d4dacd8
wellwhynot <wellwhynot@protonmail.com>
2021-02-21 06:40:58 +0000

    Added missing # for gh-user-data in the function(errorResponse

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,8 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
-    $("gh-user-data").html("");
-    $("gh-repo-data").html("");
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -47,29 +47,30 @@ function fetchGitHubInformation(event) {
     }
 
     $("#gh-user-data").html(
-        `<div id=loader>
+        `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..."/>
-        </div>`
-    );
+        </div>`);
 
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
-    ) .then(
+    ).then(
         function(firstResponse, secondResponse) {
             var userData = firstResponse[0];
             var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
             $("#gh-repo-data").html(repoInformationHTML(repoData));
-    }, function(errorResponse) {
+    }, 
+    function(errorResponse) {
         if (errorResponse.status === 404) {
-            $("gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+            $("#gh-user-data").html(
+                `<h2>No info found for user ${username}</h2>`);
         } else if (errorResponse.status === 403) {
-            var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000)
-            $("gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()} </h4>`)
+            var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+            $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
         } else {
             console.log(errorResponse);
-            $("gh-user-data").html(
+            $("#gh-user-data").html(
                 `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
         }
     });
